a1b88c986641c4faf9692bb9ea4a7dde963c8403
never2average <priyesh.sriv2017@gmail.com>
2019-06-18 14:54:23 +0530

    Fixes #5
    GeoEncoder Base

---
+++ utilities/geoencoder.py
@@ -0,0 +1,30 @@
+import requests
+
+baseURL = "https://nominatim.openstreetmap.org/?q={}&format={}&limit=1"
+
+
+class GeoEncoder:
+    def __init__(self, **kwargs):
+        if kwargs != {}:
+            try:
+                self.format = kwargs["format"]
+                try:
+                    self.callback = kwargs["callback"]
+                except Exception:
+                    print("callback is a required key word arguement.")
+            except Exception:
+                print("format is a required key word arguement.")
+        else:
+            self.format = "json"
+            self.callback = None
+
+    def fetchBoundingBox(self, address):
+        response = requests.get(baseURL.format(address, self.format)).json()
+        if self.callback is not None:
+            return self.callback(response)
+        else:
+            latlongList = response[0]["boundingbox"]
+            return [
+                latlongList[1], latlongList[2],
+                latlongList[0], latlongList[3]
+            ]
