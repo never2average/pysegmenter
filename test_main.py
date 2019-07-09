5a01c653467691aeba661326cff6b1a655abde77
Ashrut Kumar <33349624+ashrutkumar@users.noreply.github.com>
2019-07-09 13:31:32 +0530

    reflected changes done in main.py

--- test_main.py
+++ test_main.py
@@ -1,9 +1,10 @@
 from main import getimage
 
-getimage(
-		regionName = "Bangalore,Karnataka ,India",
+getimage(	regionName = "Bangalore,Karnataka ,India",
 		format = "png",
-		api_key = "Enter your BING_API_KEY here",
+		bing_api_key = "Enter your BING_API_KEY here",
+		google_api_key = "Enter your Google API Key here",
+		source_option = "Enter the source of satellite images (bing/ google)",
 		#path = "Enter the path in which the images ahould be saved",
 		debug = True
 		)
